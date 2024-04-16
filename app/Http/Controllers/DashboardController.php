386f8faa10fed135fd78af0cf9ab06e7cff34aaf
ThaeSuPaing02 <tspaing699@gmail.com>
2024-04-17 01:52:42 +0630

    search bar added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        $ideas = Idea::orderBy('created_at','desc')->paginate(3);
-        return view('dashboard',compact('ideas'));
+        $ideas = Idea::orderBy('created_at','desc');
+        if(request()->has('search')){
+            $ideas = $ideas->where('content','like','%'.request()->get('search','').'%');
+        }
+
+        return view('dashboard',[
+            'ideas' => $ideas->paginate(3)
+        ]);
     }
 }
